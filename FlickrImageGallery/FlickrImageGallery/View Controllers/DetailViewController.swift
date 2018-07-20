89534b3716175aafacec7e0f8ff9a04a03786b26
Ghulam Nasir <nasirky@gmail.com>
2018-07-20 22:22:55 +0200

    Initial setup

---
+++ FlickrImageGallery/FlickrImageGallery/View Controllers/DetailViewController.swift
@@ -0,0 +1,23 @@
+//
+//  DetailViewController.swift
+//  FlickrImageGallery
+//
+//  Created by Ghulam Nasir.
+//  Copyright © 2018 Ghulam Nasir. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
---
+++ Podfile
@@ -0,0 +1,21 @@
+workspace 'FlickrImageGallery'
+platform :ios, '11.4'
+use_frameworks!
+
+target 'FlickrImageGallery' do
+    use_frameworks!
+
+    # Podfiles
+
+
+    project 'FlickrImageGallery/FlickrImageGallery.xcodeproj'
+end
+
+target 'FlickrFetcherSDK' do
+    use_frameworks!
+    
+    #Podfiles
+
+
+    project 'FlickrFetcherSDK/FlickrFetcherSDK.xcodeproj'
+end
