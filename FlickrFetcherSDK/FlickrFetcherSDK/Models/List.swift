5f0954179ca73f68940cbea10eda2587c7dc3ac7
Ghulam Nasir <nasirky@gmail.com>
2018-07-21 23:47:39 +0200

    added sorting list items functionality in the SDK

--- FlickrFetcherSDK/FlickrFetcherSDK/Models/List.swift
+++ FlickrFetcherSDK/FlickrFetcherSDK/Models/List.swift
@@ -11,7 +11,7 @@ import Foundation
 /// Represents the list of items and the associated update time and expiry (in seconds)
 public class List {
     //MARK:- Private Members
-    private var _items: [Item]
+    private var _items: [Item]!
     private var _lastUpdated: Date
     private var _ttlInSeconds: Int
     
@@ -20,10 +20,43 @@ public class List {
     /// - Parameters:
     ///   - items: The list/array of `Item`
     ///   - ttl: The expiry time (in seconds). This property is used by `hasExpired`. The main purpose of this property is to limit the number of API(network) requests (as per the developer's preference). Setting it to 0 would force `hasExpired` to return `true` every time (asking the application to new API request)
-    public init(with items:[Item], expiresIn ttl: Int = 0) {
-        _items = items
+    public init(with items:[Item],  expiresIn ttl: Int = 0) {
         _lastUpdated = Date()
         _ttlInSeconds = ttl
+    }
+
+    /// Returns a newly initialized `List` object  with an array of `Item`, sorted by the desired sortOrder.
+    /// - Parameters:
+    ///   - items: The list/array of `Item`
+    ///   - sortOrder: The order (none, ascending, descending) to sort the items by (sorted by published date)
+    public init(with items:[Item], sortBy sortOrder: SortOrder) {
+        _lastUpdated = Date()
+        _ttlInSeconds = 0
+        
+        _items = sortOrder == .none ? items : items.sorted(by: sortOrder == .ascending ? sortAscending : sortDescending)
+    }
+
+    /// Returns a newly initialized `List` object  with an array of `Item` and the expiry time (optional with default value set to 0).
+    /// - Parameters:
+    ///   - items: The list/array of `Item`
+    ///   - sortOrder: The order (none, ascending, descending) to sort the items by. Published Date is used for this purpose.
+    ///   - ttl: The expiry time (in seconds). This property is used by `hasExpired`. The main purpose of this property is to limit the number of API(network) requests (as per the developer's preference). Setting it to 0 would force `hasExpired` to return `true` every time (asking the application to new API request)
+    public init(with items:[Item], sortBy sortOrder: SortOrder, expiresIn ttl: Int) {
+        _lastUpdated = Date()
+        _ttlInSeconds = ttl
+        
+        _items = sortOrder == .none ? items : items.sorted(by: sortOrder == .ascending ? sortAscending : sortDescending)
+    }
+    
+    //MARK:- Internal functions
+    /// Closure to sort the items in ascending order
+    func sortAscending(_ first: Item, _ second: Item) -> Bool {
+        return first.datePublished ?? Date() < second.datePublished ?? Date()
+    }
+
+    /// Closure to sort the items in descending order
+    func sortDescending(_ first: Item, _ second: Item) -> Bool {
+        return first.datePublished ?? Date() > second.datePublished ?? Date()
     }
     
     //MARK:- Public Properties
